a0b587ab2072523b2cc1dfb8caa7b3dd4687d613
Computhink-CS146 <cs146@computhink.com.sg>
2026-07-12 04:24:50 +0000

    Jul 12, 2026, 4:24 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -21,7 +21,7 @@ function draw() {
 
   // Challenge 4: A rocketship using triangle() function
   fill ("gold")
-  triangle(50,65,30,15,);
+  triangle(50,65,30,15,40,20);
 
   // Challenge 5: A square face using quad() function
 
