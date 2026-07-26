ea31b2dcc728d49d9b8e3d827fd5c9364739733a
Computhink-CS146 <cs146@computhink.com.sg>
2026-07-26 03:24:46 +0000

    Jul 26, 2026, 3:24 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,9 +1,10 @@
 function setup() {
     createCanvas (600,400)
     background ('pink')
-    function draw(){
-        for(let i = 0; i < 5;i++)
-            fill('red')
-            circle(200,300,400)
-    }
 }
+function draw(){
+     for(let i = 0; i < 5;i++)
+        fill('red')
+         circle()
+}
+
