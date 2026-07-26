e9fde6d706dd603e6ad997b3bc9ade59847d4824
Computhink-CS146 <cs146@computhink.com.sg>
2026-07-26 03:55:41 +0000

    Jul 26, 2026, 3:55 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -4,7 +4,9 @@ function setup() {
     noLoop();
 }
 let shapecolor = ('purple')
-function mousepressed(){}
+function mousepressed(){
+    
+}
 // function draw() {
 //     let xpos = 350;
 //     let ypos = 50;
