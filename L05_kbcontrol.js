9c233de8e01e13b00e662b64833cdcd350191a5d
Computhink-CS146 <cs146@computhink.com.sg>
2026-08-02 03:09:33 +0000

    Aug 2, 2026, 3:09 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -7,12 +7,12 @@ function setup() {
 }
 let shapecolor = ("white")
 function mousePressed() {
-    shapecolor = color(random(0,255),random(0,255),random(0,255))
+    shapecolor = color(random(0,255),random(0,255),random(0,255));
 
 
 
 }
 function mouseDragged(){
-    shapecolor = ('white')
+    shapecolor = ('white');
 
 }
