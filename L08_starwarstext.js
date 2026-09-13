d87aa835499b4d623619957f50a03038ae3c6ce3
Computhink-CS146 <cs146@computhink.com.sg>
2026-09-13 03:59:53 +0000

    Sep 13, 2026, 3:59 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -9,8 +9,8 @@ let storyText = [
 
 ]
 function preload(){
-    volume = loadSound('star_wars_theme_8_bit.mp3');
-    
+    volume = loadSound('sfx/star_wars_theme_8_bit.mp3');
+
 }
  function setup(){
     background(0);
