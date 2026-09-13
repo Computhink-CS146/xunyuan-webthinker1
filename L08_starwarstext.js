b7256404d98f97701be5d9d7057d4d2b6f37cdf9
Computhink-CS146 <cs146@computhink.com.sg>
2026-09-13 03:34:19 +0000

    Sep 13, 2026, 3:34 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -19,11 +19,9 @@ function draw() {
     // textAlign(LEFT);
     // text("my fav foods:")
     for(let i=0; i<FileList,length;i++) {
-        
+
         // text( (i+i) +","+list[i],50,i*50)
-    Fill(255,255,0);
-    textSize(24);
-    textAlign(CENTER);
+
    
 
     }
